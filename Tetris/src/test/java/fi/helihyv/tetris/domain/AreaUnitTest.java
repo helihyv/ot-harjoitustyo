61f1632e74acdf1e898f9c7c2f9f7a4d744de47b
Heli Hyvättinen <heli.hyvattinen@gmail.com>
2019-12-01 14:02:47 +0200

    Testejä Area-luokalle

---
+++ Tetris/src/test/java/fi/helihyv/tetris/domain/AreaUnitTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helihyv.tetris.domain;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author heli
+ */
+public class AreaUnitTest {
+    
+    Area area;
+    
+    public AreaUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        area = new Area(100,200,300,400);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void areaOverlapsReturnsTrueIfTileIsInsideArea() {
+        
+        TetrisTile tile = new TetrisTile(320,160,20);
+        
+        assertTrue(area.overlaps(tile));
+    }
+    
+    @Test
+    public void areaOverLapsReturnsFalseIfTileIsOutsideArea() {
+        TetrisTile tile = new TetrisTile(1000,1000,20);
+        
+        assertFalse(area.overlaps(tile));
+    }
+}
