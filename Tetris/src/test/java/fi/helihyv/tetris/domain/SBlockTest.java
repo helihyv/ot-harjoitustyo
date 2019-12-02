0bc10df383032ac65fa3d070f9f9c56f897d4094
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-12-02 12:22:55 +0200

    Testejä T- S- PeiliS- ja PeiliL-muotoisten palikoiden kääntymiselle

---
+++ Tetris/src/test/java/fi/helihyv/tetris/domain/SBlockTest.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.helihyv.tetris.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Heli hyvättinen
+ */
+public class SBlockTest {
+    
+    SBlock block;
+    
+    public SBlockTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        block = new SBlock(100,20);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+        @Test
+    public void sBlockRotatesCorrectlyFromZeroToNinetyDegrees() {
+        
+        block.rotate();
+        
+        Tile[] tiles = block.getTiles();
+        
+        assertEquals(120, tiles[0].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[1].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[2].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[3].getXCoordinate(), 0.1);
+        
+        assertEquals(0, tiles[0].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[1].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[2].getYCoordinate(), 0.1);
+        assertEquals(40, tiles[3].getYCoordinate(), 0.1);
+    }
+    
+        @Test
+    public void lBlockRotatesCorrectlyFromNinetyTo180Degrees() {
+        
+        block.rotate();
+        block.rotate();
+        
+        Tile[] tiles = block.getTiles();
+        
+        assertEquals(100, tiles[0].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[1].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[2].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[3].getXCoordinate(), 0.1);
+        
+        assertEquals(20, tiles[0].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[1].getYCoordinate(), 0.1);
+        assertEquals(0, tiles[2].getYCoordinate(), 0.1);
+        assertEquals(0, tiles[3].getYCoordinate(), 0.1);
+    }
+    
+        @Test
+    public void lBlockRotatesCorrectlyFrom180To270Degrees() {
+        
+        block.rotate();
+        block.rotate();
+        block.rotate();
+        
+        Tile[] tiles = block.getTiles();
+        
+        assertEquals(120, tiles[0].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[1].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[2].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[3].getXCoordinate(), 0.1);
+        
+        assertEquals(0, tiles[0].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[1].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[2].getYCoordinate(), 0.1);
+        assertEquals(40, tiles[3].getYCoordinate(), 0.1);
+    }
+    
+        @Test
+    public void lBlockRotatesCorrectlyFrom270ToZeroDegrees() {
+        
+        block.rotate();
+        block.rotate();
+        block.rotate();
+        block.rotate();
+        
+        Tile[] tiles = block.getTiles();
+        
+        assertEquals(100, tiles[0].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[1].getXCoordinate(), 0.1);
+        assertEquals(120, tiles[2].getXCoordinate(), 0.1);
+        assertEquals(140, tiles[3].getXCoordinate(), 0.1);
+        
+        assertEquals(20, tiles[0].getYCoordinate(), 0.1);
+        assertEquals(20, tiles[1].getYCoordinate(), 0.1);
+        assertEquals(0, tiles[2].getYCoordinate(), 0.1);
+        assertEquals(0, tiles[3].getYCoordinate(), 0.1);
+    }
+}
