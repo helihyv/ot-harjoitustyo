8df9365e9cbc98deb45ccded6ada9fea876162f6
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2019-12-11 19:09:05 +0200

    Korjattu I-palikan testit ja lisätty niihin bottomEdge()-funktion testaus
    I-palikan testien odotusarvot  päivitetty palikan uuden ilmestymiskorkeuden mukaisiksi.
    Lisätty testit I-palikan alareunan palauttavalle funktiolle.

--- Tetris/src/test/java/fi/helihyv/tetris/gamelogic/IBlockTest.java
+++ Tetris/src/test/java/fi/helihyv/tetris/gamelogic/IBlockTest.java
@@ -21,26 +21,26 @@ import static org.junit.Assert.*;
  * @author Heli Hyvättinen
  */
 public class IBlockTest {
-    
+
     IBlock block;
-    
+
     public IBlockTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        
-        block = new IBlock(100,20);
+
+        block = new IBlock(100, 20);
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -50,31 +50,30 @@ public class IBlockTest {
     //
     // @Test
     // public void hello() {}
-    
     @Test
     public void iBlockRotatesCorrectlyFromHorizontalToVertical() {
-        
+
         block.rotate();
-        
+
         for (int i = 0; i < 4; i++) {
-            assertEquals(120,block.getTiles()[i].getXCoordinate(),0.1);
-            assertEquals(-20 + i * 20, block.getTiles()[i].getYCoordinate(), 0.1);
+            assertEquals(120, block.getTiles()[i].getXCoordinate(), 0.1);
+            assertEquals(i * 20, block.getTiles()[i].getYCoordinate(), 0.1);
         }
-    
+
     }
-    
-    @Test 
+
+    @Test
     public void iBlockRotatesCorrectlyFromVerticalToHorizontal() {
-        
+
         block.rotate();
         block.rotate();
-        
+
         for (int i = 0; i < 4; i++) {
-            assertEquals(0,block.getTiles()[i].getYCoordinate(),0.1);
+            assertEquals(20, block.getTiles()[i].getYCoordinate(), 0.1);
             assertEquals(100 + i * 20, block.getTiles()[i].getXCoordinate(), 0.1);
         }
     }
-    
+
     @Test
     public void iBlockFreeAreasNeededToRotateTo90DegreesIncludeAreasBlockIsMovingInto() {
 
@@ -85,14 +84,14 @@ public class IBlockTest {
         boolean found3 = false;
 
         for (Area area : areas) {
-            if (area.overlaps(new TetrisTile(100, -20, 20))) {
+            if (area.overlaps(new TetrisTile(100, 0, 20))) {
                 found1 = true;
             }
-            if (area.overlaps(new TetrisTile(140, 20, 20))) {
+            if (area.overlaps(new TetrisTile(140, 40, 20))) {
                 found2 = true;
             }
 
-            if (area.overlaps(new TetrisTile(160, 40, 20))) {
+            if (area.overlaps(new TetrisTile(160, 60, 20))) {
                 found3 = true;
             }
         }
@@ -114,14 +113,14 @@ public class IBlockTest {
         boolean found3 = false;
 
         for (Area area : areas) {
-            if (area.overlaps(new TetrisTile(100, 0, 20))) {
+            if (area.overlaps(new TetrisTile(100, 20, 20))) {
                 found1 = true;
             }
-            if (area.overlaps(new TetrisTile(140, 0, 20))) {
+            if (area.overlaps(new TetrisTile(140, 20, 20))) {
                 found2 = true;
             }
 
-            if (area.overlaps(new TetrisTile(160, 0, 20))) {
+            if (area.overlaps(new TetrisTile(160, 20, 20))) {
                 found3 = true;
             }
         }
@@ -144,14 +143,14 @@ public class IBlockTest {
         boolean found3 = false;
 
         for (Area area : areas) {
-            if (area.overlaps(new TetrisTile(120, -20, 20))) {
+            if (area.overlaps(new TetrisTile(120, 0, 20))) {
                 found1 = true;
             }
-            if (area.overlaps(new TetrisTile(120, 20, 20))) {
+            if (area.overlaps(new TetrisTile(120, 40, 20))) {
                 found2 = true;
             }
 
-            if (area.overlaps(new TetrisTile(120, 40, 20))) {
+            if (area.overlaps(new TetrisTile(120, 60, 20))) {
                 found3 = true;
             }
         }
@@ -175,14 +174,14 @@ public class IBlockTest {
         boolean found3 = false;
 
         for (Area area : areas) {
-            if (area.overlaps(new TetrisTile(100, 0, 20))) {
+            if (area.overlaps(new TetrisTile(100, 20, 20))) {
                 found1 = true;
             }
-            if (area.overlaps(new TetrisTile(140, 0, 20))) {
+            if (area.overlaps(new TetrisTile(140, 20, 20))) {
                 found2 = true;
             }
 
-            if (area.overlaps(new TetrisTile(160, 0, 20))) {
+            if (area.overlaps(new TetrisTile(160, 20, 20))) {
                 found3 = true;
             }
         }
@@ -191,6 +190,43 @@ public class IBlockTest {
         assertTrue(found2);
         assertTrue(found3);
     }
-    
-}
 
+    @Test
+    public void iBlockBottomEdgeReturnsZeroWhenBlockNotOnColumn() {
+
+        assertEquals(0, block.bottomEdge(20), 0.1);
+    }
+
+    @Test
+    public void iBlockBottomEdgeReturnsCorrectYCoordinateWhenOrientationIsZeroDegrees() {
+
+        assertEquals(40, block.bottomEdge(120), 0.1);
+    }
+
+    @Test
+    public void iBlockBottomEdgeReturnsCorrectYCoordinateWhenOrientationIs90Degrees() {
+
+        block.rotate();
+        assertEquals(80, block.bottomEdge(120), 0.1);
+    }
+
+    @Test
+    public void iBlockBottomEdgeReturnsCorrectYCoordinateWhenOrientationIs180Degrees() {
+
+        block.rotate();
+        block.rotate();
+
+        assertEquals(40, block.bottomEdge(120), 0.1);
+    }
+
+    @Test
+    public void iBlockBottomEdgeReturnsCorrectYCoordinateWhenOrientationIs270Degrees() {
+
+        block.rotate();
+        block.rotate();
+        block.rotate();
+
+        assertEquals(80, block.bottomEdge(120), 0.1);
+    }
+
+}
